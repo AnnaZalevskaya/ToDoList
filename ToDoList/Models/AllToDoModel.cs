390b48235000be760148d64c2ea8c9ce5c5cd021
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2023-07-19 13:39:24 +0300

    add a model of all cases

---
+++ ToDoList/Models/AllToDoModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    public class AllToDoModel
+    {
+        public List<ToDoModel> TodoList { get; set; }
+        public ToDoModel Todo { get; set; }
+    }
+}
