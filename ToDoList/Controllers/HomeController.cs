df0541bb3dc05db2a64cec25c75c497c5e3be531
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2023-07-19 13:08:19 +0300

    add insert method

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
@@ -26,5 +28,28 @@ namespace ToDoList.Controllers
 
             return View();
         }
+
+        public RedirectResult Insert(ToDoModel todo)
+        {
+            using (SqlConnection con =
+                   new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\ToDoDB.mdf;Integrated Security=True"))
+            {
+                using (var tableCmd = con.CreateCommand())
+                {
+                    con.Open();
+                    tableCmd.CommandText = $"INSERT INTO [ToDo] (Name) VALUES ('{todo.Name}')";
+                    try
+                    {
+                        tableCmd.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+            return Redirect("https://localhost:5001/");
+        }
+
     }
 }
