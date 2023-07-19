f96903a44016f698e7cbb2201e9f1d7b511f0b73
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2023-07-19 15:52:19 +0300

    edit js file

--- ToDoList/Scripts/site.js
+++ ToDoList/Scripts/site.js
@@ -6,7 +6,25 @@
             id: i
         },
         success: function () {
-            window.location.reload();
+            Window.location.reload();
+        }
+    });
+}
+
+function populateForm(i) {
+
+    $.ajax({
+        url: 'Home/PopulateForm',
+        type: 'GET',
+        data: {
+            id: i
+        },
+        dataType: 'json',
+        success: function (response) {
+            $("#Todo_Name").val(response.name);
+            $("#Todo_Id").val(response.id);
+            $("#form-button").val("Update Todo");
+            $("#form-action").attr("action", "/Home/Update");
         }
     });
 }
