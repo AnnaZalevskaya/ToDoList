b8d5037d41cc6d75ff415e2b00cf147aeff9b6ec
AnnaZalevskaya <123122231+AnnaZalevskaya@users.noreply.github.com>
2023-07-19 13:54:24 +0300

    Create forms (#2)
    * add main model
    
    * add new layout
    
    * create form and add
    
    * deleting unnecessary files
    
    * add insert method
    
    * add a model of all cases
    
    * add get all cases method
    
    * fix forms
    
    * edit the code style

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
@@ -10,7 +12,9 @@ namespace ToDoList.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var todoListViewModel = GetAllTodos();
+
+            return View(todoListViewModel);
         }
 
         public ActionResult About()
@@ -26,5 +30,71 @@ namespace ToDoList.Controllers
 
             return View();
         }
+
+        public RedirectResult Insert(ToDoModel todo)
+        {
+            using (SqlConnection con =
+                   new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\ToDoDB.mdf;Integrated Security=True"))
+            {
+                using (var tableCmd = con.CreateCommand())
+                {
+                    con.Open();
+                    tableCmd.CommandText = $"INSERT INTO [ToDo] (Name) VALUES ('{todo.Name}')";
+                    try
+                    {
+                        tableCmd.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+            return Redirect("https://localhost:5001/");
+        }
+
+        public AllToDoModel GetAllTodos()
+        {
+            List<ToDoModel> todoList = new List<ToDoModel>();
+
+            using (SqlConnection con =
+                   new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\ToDoDB.mdf;Integrated Security=True"))
+            {
+                using (var tableCmd = con.CreateCommand())
+                {
+                    con.Open();
+                    tableCmd.CommandText = "SELECT * FROM [ToDo]";
+
+                    using (var reader = tableCmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                todoList.Add(
+                                    new ToDoModel
+                                    {
+                                        Id = reader.GetInt32(0),
+                                        Name = reader.GetString(1)
+                                    });
+                            }
+                        }
+                        else
+                        {
+                            return new AllToDoModel
+                            {
+                                TodoList = todoList
+                            };
+                        }
+                    };
+                }
+            }
+
+            return new AllToDoModel
+            {
+                TodoList = todoList
+            };
+        }
+
     }
 }
